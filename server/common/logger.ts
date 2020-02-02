3cd2ea2f8559c83f7759a5ab88169fd880c2ef49
Michael Bowen <mikee.bowen@gmail.com>
2020-02-01 16:01:10 -0800

    have logger write to file

--- server/common/logger.ts
+++ server/common/logger.ts
@@ -1,8 +1,20 @@
 import pino from 'pino';
+import {existsSync, writeFile} from 'fs';
+import {forEach} from 'lodash';
+// import '../../log/dev-log';
+import {join} from 'path';
+const nodeEnv = process.env.NODE_ENV?.toLowerCase();
+const logFilePath = join(__dirname, '..', '..', 'log', nodeEnv === 'production' ||  nodeEnv === 'prod' ? 'log' : 'dev-log');
+
+if (!existsSync(logFilePath)) {
+  writeFile(logFilePath, '', err => {
+    if (err) throw err;
+  });
+}
 
 const l = pino({
   name: process.env.APP_ID,
   level: process.env.LOG_LEVEL,
-});
+}, pino.destination(logFilePath));
 
 export default l;
