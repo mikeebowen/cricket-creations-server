3d53567546035e07c9a8c7a847074cd2cdba4d3e
Michael Bowen <mikee.bowen@gmail.com>
2020-02-15 19:09:46 -0800

    move .env file to root

--- server/db/config/config.js
+++ server/db/config/config.js
@@ -1,4 +1,7 @@
-require('dotenv').config();
+import dotenv from 'dotenv';
+import {join} from 'path';
+
+dotenv.config({path: join(__dirname, '..', '..', '..', '.env')});
 
 module.exports = {
   development: {
