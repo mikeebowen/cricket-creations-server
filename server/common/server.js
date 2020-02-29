8f80cda55ad4ae58e4812df2b628d03adcf07086
Michael Bowen <mikee.bowen@gmail.com>
2020-02-29 10:04:12 -0800

    remove express-openapi-validator

--- server/common/server.js
+++ server/common/server.js
@@ -4,23 +4,20 @@ import {json, urlencoded, text} from 'body-parser';
 import {createServer} from 'http';
 import {hostname} from 'os';
 import cookieParser from 'cookie-parser';
-
-import oas from './oas';
+import errorHandler from '../api/middlewares/error.handler';
 
 import l from './logger';
 
 const app = new Express();
-const {exit} = process;
-
+const {REQUEST_LIMIT, SESSION_SECRET, PORT, NODE_ENV} = process.env;
 export default class ExpressServer {
   constructor() {
     const root = normalize(`${__dirname}/../..`);
     app.set('appPath', `${root}client`);
-    app.use(json({limit: process.env.REQUEST_LIMIT || '100kb'}));
-    app.use(urlencoded({extended: true, limit: process.env.REQUEST_LIMIT || '100kb'}));
-    app.use(text({limit: process.env.REQUEST_LIMIT || '100kb'}));
-    app.use(cookieParser(process.env.SESSION_SECRET));
-    app.use(Express.static(`${root}/public`));
+    app.use(json({limit: REQUEST_LIMIT || '100kb'}));
+    app.use(urlencoded({extended: true, limit: REQUEST_LIMIT || '100kb'}));
+    app.use(text({limit: REQUEST_LIMIT || '100kb'}));
+    app.use(cookieParser(SESSION_SECRET));
   }
 
   router(routes) {
@@ -28,18 +25,12 @@ export default class ExpressServer {
     return this;
   }
 
-  listen(port = process.env.PORT) {
-    const welcome = p => () => l.info(
-      `up and running in ${process.env.NODE_ENV
-          || 'development'} @: ${hostname()} on port: ${p}}`,
-    );
+  listen(port = PORT) {
+    const welcome = p => () => l.info(`up and running in ${NODE_ENV || 'development'} @: ${hostname()} on port: ${p}}`);
 
-    oas(app, this.routes).then(() => {
-      createServer(app).listen(port, welcome(port));
-    }).catch(e => {
-      l.error(e);
-      exit(1);
-    });
+    createServer(app).listen(port, welcome(port));
+    this.routes(app);
+    app.use(errorHandler);
     return app;
   }
 }
